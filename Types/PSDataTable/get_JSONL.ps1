d3fb994b240316f522a4351166e86e972bd95d6c
James Brundage <@github.com>
2024-07-21 01:17:43 -0700

    feat: PSDataTable.get_JSONL ( Fixes #35 )

---
+++ Types/PSDataTable/get_JSONL.ps1
@@ -0,0 +1,11 @@
+<#
+.SYNOPSIS
+    Gets a datatable as JSONL
+.DESCRIPTION
+    Gets a JSONL representation of a data table
+#>
+param()
+if ($this -isnot [Data.DataTable]) { return }
+if (-not $this.Columns.Count) { return }
+$columnNames = @($this.Columns.ColumnName)
+$this | Select-Object -Property $columnNames | . {process { $_  | ConvertTo-Json -Compress } }
