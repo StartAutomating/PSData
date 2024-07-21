537bfc2de8aa3f8f7c8d1423c7a953e6885c24ba
James Brundage <@github.com>
2024-07-21 01:52:05 -0700

    feat: PSDataTable.get_JSON ( Fixes #34 )

---
+++ Types/PSDataTable/get_JSON.ps1
@@ -0,0 +1,11 @@
+<#
+.SYNOPSIS
+    Gets a data table as JSON
+.DESCRIPTION
+    Gets a JSON representation of a data table
+#>
+param()
+if ($this -isnot [Data.DataTable]) { return }
+if (-not $this.Columns.Count) { return }
+$columnNames = @($this.Columns.ColumnName)
+$this | Select-Object -Property $columnNames | ConvertTo-Json
