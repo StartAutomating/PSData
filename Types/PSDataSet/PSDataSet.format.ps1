1493f7767afd02f5bb5f85ea0d944a7ffe97bf9c
James Brundage <@github.com>
2024-07-21 00:40:23 -0700

    feat: PSDataSet formatting ( Fixes #17 )
    Grouping by DataSetName and not showing Namespace

--- Types/PSDataSet/PSDataSet.format.ps1
+++ Types/PSDataSet/PSDataSet.format.ps1
@@ -1,3 +1,3 @@
-Write-FormatView -TypeName 'PSDataSet', 'System.Data.DataSet' -Property Namespace, TableName -VirtualProperty @{
+Write-FormatView -TypeName 'PSDataSet', 'System.Data.DataSet' -Property TableName -VirtualProperty @{
     TableName = { $_.TableName -join [Environment]::NewLine }    
 } -Wrap -Name Default -GroupByProperty DataSetName
