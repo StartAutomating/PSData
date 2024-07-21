f3ace6905881917ab9889988327d29e46cb7e059
James Brundage <@github.com>
2024-07-21 00:15:58 -0700

    feat: PSDataColumn Formatting ( Fixes #26 )
    -GroupByProperty, not -GroupBy

--- Types/PSDataColumn/PSDataColumn.format.ps1
+++ Types/PSDataColumn/PSDataColumn.format.ps1
@@ -1,1 +1,1 @@
-Write-FormatView -TypeName PSDataColumn, 'System.Data.DataColumn' -Property ColumnName, DataType, AutoIncrement, DefaultValue, Expression -AutoSize -Name Basic -GroupBy TableName
+Write-FormatView -TypeName PSDataColumn, 'System.Data.DataColumn' -Property ColumnName, DataType, AutoIncrement, DefaultValue, Expression -AutoSize -Name Basic -GroupByProperty TableName
