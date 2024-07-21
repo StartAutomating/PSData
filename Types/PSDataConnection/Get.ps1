084074676441c690fb25c98ebf6d7d9afabb67c1
James Brundage <@github.com>
2024-07-21 00:57:37 -0700

    feat: PSDataConnection.Get() ( Fixes #32 )

---
+++ Types/PSDataConnection/Get.ps1
@@ -0,0 +1,15 @@
+<#
+.SYNOPSIS
+    Gets data connections
+.DESCRIPTION
+    Gets loaded data connections.
+.NOTES
+    Will only get connections that have been saved to a variable, and are a `[Data.Common.DbConnection]`.
+#>
+param()
+
+foreach ($variable in @(Get-Variable)) {
+    if ($variable.Value -is [Data.Common.DbConnection]) {
+        $variable.Value
+    }
+}
