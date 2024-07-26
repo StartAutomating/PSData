fc73bcb8cd0e0d93b1cbda03e9490c9c9701ef2e
James Brundage <@github.com>
2024-07-25 19:32:34 -0700

    feat: Get-PSDataConnection ( Fixes #32 )

--- Commands/PSDataConnection/Get-PSDataConnection.ps1
+++ Commands/PSDataConnection/Get-PSDataConnection.ps1
@@ -1,15 +1,18 @@
-<#
-.SYNOPSIS
-    Gets data connections
-.DESCRIPTION
-    Gets loaded data connections.
-.NOTES
-    Will only get connections that have been saved to a variable, and are a `[Data.Common.DbConnection]`.
-#>
-param()
+function Get-PSDataConnection {
+    <#
+    .SYNOPSIS
+        Gets data connections
+    .DESCRIPTION
+        Gets loaded data connections.
+    .NOTES
+        Will only get connections that have been saved to a variable, and are a `[Data.Common.DbConnection]`.
+    #>
+    [Alias('Get-DataConnection')]
+    param()
 
-foreach ($variable in @(Get-Variable)) {
-    if ($variable.Value -is [Data.Common.DbConnection]) {
-        $variable.Value
+    foreach ($variable in @(Get-Variable)) {
+        if ($variable.Value -is [Data.Common.DbConnection]) {
+            $variable.Value
+        }
     }
 }
