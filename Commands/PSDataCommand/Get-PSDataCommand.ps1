686fcd10cf87fb86b871fedf5acf6b2572f88e6a
James Brundage <@github.com>
2024-07-25 19:28:34 -0700

    feat: Get-PSDataCommand ( Fixes #30 )

---
+++ Commands/PSDataCommand/Get-PSDataCommand.ps1
@@ -0,0 +1,19 @@
+function Get-PSDataCommand 
+{
+    <#
+    .SYNOPSIS
+        Gets data commands
+    .DESCRIPTION
+        Gets loaded data commands.
+    .NOTES
+        Will only get commands that have been saved to a variable, and are a `[Data.Common.DbCommand]`.
+    #>
+    [Alias('Get-DataCommand')]
+    param()
+
+    foreach ($variable in @(Get-Variable)) {
+        if ($variable.Value -is [Data.Common.DbCommand]) {
+            $variable.Value
+        }
+    }
+}
