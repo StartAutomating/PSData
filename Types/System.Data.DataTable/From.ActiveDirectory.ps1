7effc8396af55eb84abb1e444621a08cbf14adc4
James Brundage <@github.com>
2024-07-30 23:39:47 -0700

    feat: System.Data.DataTable.From.ActiveDirectory ( Fixes #81 )

---
+++ Types/System.Data.DataTable/From.ActiveDirectory.ps1
@@ -0,0 +1,50 @@
+param(
+# The directory search
+[PSObject]
+$Search
+)
+
+if (-not ('Directory.DirectorySearcher' -as [Type])) {
+    Write-Warning "The 'System.DirectoryServices.DirectorySearcher' type is not available.  Only deserialized LDAP queries will be allowed."    
+} else {
+    if ($search -isnot [Directory.DirectorySearcher] -and $search -isnot [Directory.SearchResult]) {
+        if ($search -is [Collections.IDictionary]) {
+            $searchQuery = "($(@(foreach ($keyValue in $search.GetEnumerator()) {
+                "($($keyValue.Key)=$($keyValue.Value))"
+            })))"
+            $search = @([Directory.DirectorySearcher]::new($searchQuery).FindAll())
+
+        }
+    } 
+}
+
+if ($this -and $this -is [Data.DataTable]) {
+    $NewTable = $this 
+} else {
+    $NewTable = New-PSDataTable -Column LDAP -Key LDAP
+}
+
+foreach ($item in $search) {
+    if (-not ($item.LDAP -and $item.Properties)) {
+        continue
+    }
+    $itemProperties = [Ordered]@{} + $item.Properties
+    $newRow = $NewTable.NewRow()
+    $newRow['LDAP'] = $item.LDAP
+    foreach ($itemKeyValue in $itemProperties.GetEnumerator()) {
+        $columnName = $itemKeyValue.Key
+        if (-not $NewTable.Columns[$columnName]) {
+            $columnType = $itemKeyValue.GetType()
+            $newColumnSplat = [Ordered]@{
+                ColumnName = $columnName
+                ColumnType = $columnType
+            }
+            $newColumn = New-PSDataColumn @newColumnSplat
+            $NewTable.Columns.Add($newColumn)
+        }
+        $newRow[$columnName] = $itemKeyValue.Value
+    }
+}
+
+return ,$NewTable
+
