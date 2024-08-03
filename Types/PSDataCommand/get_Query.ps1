4a262839e59dd6b6e5fffbd2ada74c9a91b5ed0a
James Brundage <@github.com>
2024-08-03 14:17:03 -0700

    feat: PSDataCommand.get_Query ( Fixes #85, Fixes #49 )
    Returning .Query as PSDataQuery

--- Types/PSDataCommand/get_Query.ps1
+++ Types/PSDataCommand/get_Query.ps1
@@ -1,8 +1,18 @@
 if ($this -is [Data.Common.DbCommand]) {
-    return $this.CommandText
+    return [PSCustomObject]@{
+        PSTypeName='PSDataQuery'
+        Query=$this.CommandText
+        Language='SQL'
+        Command=$this
+    }     
 }
 elseif ($this -is [ScriptBlock]) {
-    return $this.ToString()
+    return [PSCustomObject]@{
+        PSTypeName='PSDataQuery'
+        Query=$this.ToString()
+        Language='PowerShell'
+        Command=$this
+    }
 }
 elseif (
     $this -is [Management.Automation.CommandInfo] -or 
@@ -10,6 +20,12 @@ elseif (
     $this -is [Mangement.Automation.PSMethodInfo] -or
     $this -is [Reflection.MemberInfo]
 ) {
-    return $this.Name
+    return [PSCustomObject]@{
+        PSTypeName='PSDataQuery'
+        Query=$this.Name
+        Language='PowerShell'
+        Command=$this
+    }
+    return 
 }
 
